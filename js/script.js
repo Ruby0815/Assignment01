e21fb2fbac76803149ffec5099365d07881c1bce
Ruby Verma <rubyverma@Rubys-MacBook-Air.local>
2023-06-25 15:59:35 -0700

    Update script.js
    step 9 is added in script file

--- js/script.js
+++ js/script.js
@@ -24,7 +24,8 @@ let strFirstName;
  let intYourAge;
  let strRefSource;
  let boolMayWeContactYou;
-//step 5
+
+ //step 5
 //option 1  
 let strfirstName = "John";
 let strlastName = "Smith";
@@ -36,18 +37,27 @@ let $firstName, $lastName, $yourAge;
 strFirstName = "Mary";
 strLastName = "Smith";
 intYourAge = 50;
+
 //step 6
 let classJoining;
 classJoining = 2; 
 console.log("I joined for "+classJoining+" year");
+
 //step 7
 let var1;
 let var2;
 var1 = true +" hello " ;//Boolean is coerced to a string
-
 console.log(var1)
 var2 = 2 + false;//Boolean is coerced to a  number
 console.log(var2)
+
 //step 8
 let someString = "Who once said,'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
 window.console.log(someString);
+
+//step 9
+let a;
+a=null;
+console.log("a = " +a);
+let b;
+console.log("b = " +b);
