861c6b1d3ae2fcbcd71b18e70b1d4cfcae496844
Ruby Verma <rubyverma@Rubys-MacBook-Air.local>
2023-06-25 15:46:28 -0700

    Update script.js
    some variable is declared in script file using  Camel Casing and Hungarian Notation

--- js/script.js
+++ js/script.js
@@ -14,3 +14,13 @@ let varExpression1 = 2 * (40 / 8) + 5;
 let varExpression2 = 3 + (8 % 2) - 1;
 console.log("varExpression1:= "+varExpression1);
 console.log("varExpression2:= "+varExpression2);
+//step 4
+let strFirstName;
+ let strLastName;
+ let strAddress;
+ let strCity;
+ let strState;
+ let intZipCode;
+ let intYourAge;
+ let strRefSource;
+ let boolMayWeContactYou;
